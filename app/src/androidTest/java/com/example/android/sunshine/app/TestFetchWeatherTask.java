d030f32ddf9c0fae332b872873bd3d93eb089921
asimq74 <asim.m.qureshi@gmail.com>
2016-09-22 09:42:59 -0400

    halfway through testprovider

--- app/src/androidTest/java/com/example/android/sunshine/app/TestFetchWeatherTask.java
+++ app/src/androidTest/java/com/example/android/sunshine/app/TestFetchWeatherTask.java
@@ -17,14 +17,15 @@ package com.example.android.sunshine.app;
 
 import android.test.AndroidTestCase;
 
-public class TestFetchWeatherTask extends AndroidTestCase{
-    static final String ADD_LOCATION_SETTING = "Sunnydale, CA";
-    static final String ADD_LOCATION_CITY = "Sunnydale";
-    static final double ADD_LOCATION_LAT = 34.425833;
-    static final double ADD_LOCATION_LON = -119.714167;
+public class TestFetchWeatherTask extends AndroidTestCase {
+
+	static final String ADD_LOCATION_CITY = "Sunnydale";
+	static final double ADD_LOCATION_LAT = 34.425833;
+	static final double ADD_LOCATION_LON = -119.714167;
+	static final String ADD_LOCATION_SETTING = "Sunnydale, CA";
 
     /*
-        Students: uncomment testAddLocation after you have written the AddLocation function.
+				Students: uncomment testAddLocation after you have written the AddLocation function.
         This test will only run on API level 11 and higher because of a requirement in the
         content provider.
      */
--- app/src/androidTest/java/com/example/android/sunshine/app/data/TestWeatherContract.java
+++ app/src/androidTest/java/com/example/android/sunshine/app/data/TestWeatherContract.java
@@ -19,14 +19,14 @@ import android.net.Uri;
 import android.test.AndroidTestCase;
 
 /*
-    Students: This is NOT a complete test for the WeatherContract --- just for the functions
+		Students: This is NOT a complete test for the WeatherContract --- just for the functions
     that we expect you to write.
  */
 public class TestWeatherContract extends AndroidTestCase {
 
-    // intentionally includes a slash to make sure Uri is getting quoted correctly
-    private static final String TEST_WEATHER_LOCATION = "/North Pole";
-    private static final long TEST_WEATHER_DATE = 1419033600L;  // December 20th, 2014
+	private static final long TEST_WEATHER_DATE = 1419033600L;  // December 20th, 2014
+	// intentionally includes a slash to make sure Uri is getting quoted correctly
+	private static final String TEST_WEATHER_LOCATION = "/North Pole";
 
     /*
         Students: Uncomment this out to test your weather location function.
