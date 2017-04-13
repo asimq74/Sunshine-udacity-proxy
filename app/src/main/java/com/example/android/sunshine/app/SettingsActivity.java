d0c7db4b8fa9501d7921ef053bc8c6bd411525e0
asimq74 <asim.m.qureshi@gmail.com>
2017-04-12 20:28:37 -0400

    Change icon pack based on user preference

--- app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
@@ -37,123 +37,127 @@ import com.example.android.sunshine.app.sync.SunshineSyncAdapter;
  * API Guide</a> for more information on developing a Settings UI.
  */
 public class SettingsActivity extends PreferenceActivity
-		implements Preference.OnPreferenceChangeListener, SharedPreferences.OnSharedPreferenceChangeListener {
+        implements Preference.OnPreferenceChangeListener, SharedPreferences.OnSharedPreferenceChangeListener {
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		// Add 'general' preferences, defined in the XML file
-		addPreferencesFromResource(R.xml.pref_general);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Add 'general' preferences, defined in the XML file
+        addPreferencesFromResource(R.xml.pref_general);
 
-		// For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
-		// updated when the preference changes.
-		bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
-		bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_units_key)));
-	}
+        // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
+        // updated when the preference changes.
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_units_key)));
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_art_pack_key)));
+    }
 
-	// Registers a shared preference change listener that gets notified when preferences change
-	@Override
-	protected void onResume() {
-		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-		sp.registerOnSharedPreferenceChangeListener(this);
-		super.onResume();
-	}
+    // Registers a shared preference change listener that gets notified when preferences change
+    @Override
+    protected void onResume() {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        sp.registerOnSharedPreferenceChangeListener(this);
+        super.onResume();
+    }
 
-	// Unregisters a shared preference change listener
-	@Override
-	protected void onPause() {
-		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-		sp.unregisterOnSharedPreferenceChangeListener(this);
-		super.onPause();
-	}
+    // Unregisters a shared preference change listener
+    @Override
+    protected void onPause() {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        sp.unregisterOnSharedPreferenceChangeListener(this);
+        super.onPause();
+    }
 
-	/**
-	 * Attaches a listener so the summary is always updated with the preference value.
-	 * Also fires the listener once, to initialize the summary (so it shows up before the value
-	 * is changed.)
-	 */
-	private void bindPreferenceSummaryToValue(Preference preference) {
-		// Set the listener to watch for value changes.
-		preference.setOnPreferenceChangeListener(this);
+    /**
+     * Attaches a listener so the summary is always updated with the preference value.
+     * Also fires the listener once, to initialize the summary (so it shows up before the value
+     * is changed.)
+     */
+    private void bindPreferenceSummaryToValue(Preference preference) {
+        // Set the listener to watch for value changes.
+        preference.setOnPreferenceChangeListener(this);
 
-		// Set the preference summaries
-		setPreferenceSummary(preference,
-				PreferenceManager
-						.getDefaultSharedPreferences(preference.getContext())
-						.getString(preference.getKey(), ""));
-	}
+        // Set the preference summaries
+        setPreferenceSummary(preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
+    }
 
-	private void setPreferenceSummary(Preference preference, Object value) {
-		String stringValue = value.toString();
-		String key = preference.getKey();
+    private void setPreferenceSummary(Preference preference, Object value) {
+        String stringValue = value.toString();
+        String key = preference.getKey();
 
-		if (preference instanceof ListPreference) {
-			// For list preferences, look up the correct display value in
-			// the preference's 'entries' list (since they have separate labels/values).
-			ListPreference listPreference = (ListPreference) preference;
-			int prefIndex = listPreference.findIndexOfValue(stringValue);
-			if (prefIndex >= 0) {
-				preference.setSummary(listPreference.getEntries()[prefIndex]);
-			}
-		} else if (key.equals(getString(R.string.pref_location_key))) {
-			@SunshineSyncAdapter.LocationStatus int status = Utility.getLocationStatus(this);
-			switch (status) {
-				case SunshineSyncAdapter.LOCATION_STATUS_OK:
-					preference.setSummary(stringValue);
-					break;
-				case SunshineSyncAdapter.LOCATION_STATUS_UNKNOWN:
-					preference.setSummary(getString(R.string.pref_location_unknown_description, value.toString()));
-					break;
-				case SunshineSyncAdapter.LOCATION_STATUS_INVALID:
-					preference.setSummary(getString(R.string.pref_location_error_description, value.toString()));
-					break;
-				case SunshineSyncAdapter.LOCATION_STATUS_SERVER_INVALID:
-					preference.setSummary(getString(R.string.pref_location_error_description, value.toString()));
-					break;
-				case SunshineSyncAdapter.LOCATION_STATUS_SERVER_DOWN:
-					preference.setSummary(getString(R.string.pref_location_error_description, value.toString()));
-					break;
-				default:
-					// Note --- if the server is down we still assume the value
-					// is valid
-					preference.setSummary(stringValue);
-			}
-		} else {
-			// For other preferences, set the summary to the value's simple string representation.
-			preference.setSummary(stringValue);
-		}
+        if (preference instanceof ListPreference) {
+            // For list preferences, look up the correct display value in
+            // the preference's 'entries' list (since they have separate labels/values).
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            if (prefIndex >= 0) {
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else if (key.equals(getString(R.string.pref_location_key))) {
+            @SunshineSyncAdapter.LocationStatus int status = Utility.getLocationStatus(this);
+            switch (status) {
+                case SunshineSyncAdapter.LOCATION_STATUS_OK:
+                    preference.setSummary(stringValue);
+                    break;
+                case SunshineSyncAdapter.LOCATION_STATUS_UNKNOWN:
+                    preference.setSummary(getString(R.string.pref_location_unknown_description, value.toString()));
+                    break;
+                case SunshineSyncAdapter.LOCATION_STATUS_INVALID:
+                    preference.setSummary(getString(R.string.pref_location_error_description, value.toString()));
+                    break;
+                case SunshineSyncAdapter.LOCATION_STATUS_SERVER_INVALID:
+                    preference.setSummary(getString(R.string.pref_location_error_description, value.toString()));
+                    break;
+                case SunshineSyncAdapter.LOCATION_STATUS_SERVER_DOWN:
+                    preference.setSummary(getString(R.string.pref_location_error_description, value.toString()));
+                    break;
+                default:
+                    // Note --- if the server is down we still assume the value
+                    // is valid
+                    preference.setSummary(stringValue);
+            }
+        } else {
+            // For other preferences, set the summary to the value's simple string representation.
+            preference.setSummary(stringValue);
+        }
 
-	}
+    }
 
-	// This gets called before the preference is changed
-	@Override
-	public boolean onPreferenceChange(Preference preference, Object value) {
-		setPreferenceSummary(preference, value);
-		return true;
-	}
+    // This gets called before the preference is changed
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+        setPreferenceSummary(preference, value);
+        return true;
+    }
 
-	// This gets called after the preference is changed, which is important because we
-	// start our synchronization here
-	@Override
-	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-		if ( key.equals(getString(R.string.pref_location_key)) ) {
-			// we've changed the location
-			// first clear locationStatus
-			Utility.resetLocationStatus(this);
-			SunshineSyncAdapter.syncImmediately(this);
-		} else if ( key.equals(getString(R.string.pref_units_key)) ) {
-			// units have changed. update lists of weather entries accordingly
-			getContentResolver().notifyChange(WeatherContract.WeatherEntry.CONTENT_URI, null);
-		} else if ( key.equals(getString(R.string.pref_location_status_key)) ) {
-			// our location status has changed.  Update the summary accordingly
-			Preference locationPreference = findPreference(getString(R.string.pref_location_key));
-			bindPreferenceSummaryToValue(locationPreference);
-		}
-	}
+    // This gets called after the preference is changed, which is important because we
+    // start our synchronization here
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(getString(R.string.pref_location_key))) {
+            // we've changed the location
+            // first clear locationStatus
+            Utility.resetLocationStatus(this);
+            SunshineSyncAdapter.syncImmediately(this);
+        } else if (key.equals(getString(R.string.pref_units_key))) {
+            // units have changed. update lists of weather entries accordingly
+            getContentResolver().notifyChange(WeatherContract.WeatherEntry.CONTENT_URI, null);
+        } else if (key.equals(getString(R.string.pref_location_status_key))) {
+            // our location status has changed.  Update the summary accordingly
+            Preference locationPreference = findPreference(getString(R.string.pref_location_key));
+            bindPreferenceSummaryToValue(locationPreference);
+        } else if (key.equals(getString(R.string.pref_art_pack_key))) {
+            // art pack have changed. update lists of weather entries accordingly
+            getContentResolver().notifyChange(WeatherContract.WeatherEntry.CONTENT_URI, null);
+        }
+    }
 
-	@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-	@Override
-	public Intent getParentActivityIntent() {
-		return super.getParentActivityIntent().addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-	}
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public Intent getParentActivityIntent() {
+        return super.getParentActivityIntent().addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    }
 }
